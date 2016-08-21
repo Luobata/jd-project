00d531f42e0bc41176d015ef66dee5a96ed68dcf
Luobta <602975805@qq.com>
2016-08-22 00:03:02 +0800

    add: init
    Signed-off-by: Luobta <602975805@qq.com>

---
+++ js/index.js
@@ -0,0 +1,15 @@
+define(function(require, exports) {
+    var module = {};
+    var dom = {};
+
+    var domInit = function () {
+    };
+
+    var eventBind = function () {
+    };
+
+    var init = function () {
+    };
+
+    init();
+});

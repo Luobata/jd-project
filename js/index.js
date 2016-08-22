6d3ac2548d48814125407666322085f242bd7df9
Luobta <602975805@qq.com>
2016-08-23 00:21:50 +0800

    fix: px2em.
    Signed-off-by: Luobta <602975805@qq.com>

--- js/index.js
+++ js/index.js
@@ -8,7 +8,22 @@ define(function(require, exports) {
     var eventBind = function () {
     };
 
+    var enter = function () {
+        var $index = $('#index');
+        var ua = window.navigator.userAgent;
+        var isMobile = ua.match(/(Android|iPhone|iPad)/);
+        if (isMobile) {
+            $index.css('font-family', '"Helvetica"');
+            $index.css('font-family', '"黑体-简"');
+        } else {
+            $index.css('font-family', '"Helvetica", "黑体-简"');
+        }
+        var width = document.body.clientWidth;
+        $('html').css('font-size', width * 12 / 320 + 'px');
+    };
+
     var init = function () {
+        enter();
     };
 
     init();
